cd0b6813a126ab9c03cc27abc6c871ac4a529241
Juan Pablo Acosta <113045937+JPablo67@users.noreply.github.com>
2024-09-22 22:05:09 +0000

    resposive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -29,7 +29,7 @@ const Home = ({ name, title }) => {
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
-        <h1>{name}</h1>
+        <h1 style={{ marginRight: "20px", width:"500px" }}>{name}</h1> {/* Adjust margin or padding here */}
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
